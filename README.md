3a8bad4db302735ba3fc0899396348d1f83c4e51
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-25 13:18:17 -0400

    Initial commit, including types from @pxblue/colors

---
+++ README.md
@@ -0,0 +1,9 @@
+# PX Blue Types
+
+This repository contains common type definitions used within various PX Blue packages.
+This package can be viewed on NPM as [@pxblue/types](https://www.npmjs.com/package/@pxblue/types)
+
+### Build
+This package can be built by running:
+
+```yarn build```
---
+++ buildTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
+
+echo "Checking for required files..."
+echo -ne "index.js: "
+if [ ! -f ./dist/index.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "index.d.ts: "
+if [ ! -f ./dist/index.d.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "README.md: "
+if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "LICENSE: "
+if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "package.json: "
+if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+
+echo -e "\r\n${GREEN}-----------------------------------"
+echo -e "Color packages successfully created"
+echo -e "-----------------------------------${NC}\r\n\r\n"
+
+exit 0
